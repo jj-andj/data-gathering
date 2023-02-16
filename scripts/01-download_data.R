378fdcf6d45dd6b963afea254f71f9d1d950ae47
Jayden Jung <jayden.jung@mail.utoronto.ca>
2023-02-16 11:02:03 -0500

    Get data from NASA API

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,20 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
+library(httr)
+library(xml2)
 # [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+NASA_APOD_20030309 <- GET("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2003-03-09")
 
-
+content(NASA_APOD_20030309)$url %>% 
+  download.file(destfile = "inputs/NASA_APOD_20030309.jpg")
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
